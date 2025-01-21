9fa726ae672055b80d08bd8f0584abec1d8561e6
Pratyush Kumar <124339540+pratyushk512@users.noreply.github.com>
2025-01-21 12:16:16 +0530

    Update constants.js

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,5 +1,5 @@
 import axios from "axios"
-export const baseURL = "http://localhost:8000/api/v1"
+export const baseURL = "https://career-advisor-server-omega.vercel.app/api/v1"
 export const api = axios.create({
     baseURL: baseURL,
     withCredentials: true, 
